970a736449ec148011bacce21da55e4bfa14a132
Luca Alfino <alfinoluca21@gmail.com>
2024-03-14 08:41:57 +0100

    Fixed python directory structure

--- PYTHON/Algorithms/Factorial/main.py
+++ PYTHON/Algorithms/Factorial/main.py
@@ -1,10 +1,15 @@
+
 def factorial(num):
     if num <= 1:
         return 1
     else:
         return num * factorial(num - 1)
 
+
+# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     num = 5
     result = factorial(num)
     print(result)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
