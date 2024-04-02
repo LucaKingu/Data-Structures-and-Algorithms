784fd212432f522d050fead091e4207c92f1dec5
Luca Alfino <alfinoluca21@gmail.com>
2024-04-02 11:56:07 +0200

    Binary search for C++

--- C++/Algorithms/Search Algorithms/Binary Search/Binary Search/Binary Search.cpp
+++ C++/Algorithms/Search Algorithms/Binary Search/Binary Search/Binary Search.cpp
@@ -1,11 +1,40 @@
 #include <iostream>
 
-int main()
+
+int binarySearch(const int array[], int size ,int element)
 {
-    std::cout << "Hello World!\n";
+    int low = 0;
+    int high = size - 1;
+
+    while (low <= high)
+    {
+        int midIndex = (low + high) / 2;
+        int midValue = array[midIndex];
+
+        if (element == midValue)
+        {
+            return midIndex;
+        }
+
+        if (element < midValue)
+        {
+            high = midIndex - 1;
+        }
+        else
+        {
+            low = midIndex + 1;
+        }
+    }
+    return -1;
 }
 
-int binarySearch(const int (&array)[], int element)
+int main()
 {
+    //Array MUST be sorted for Binary search to work
+    int array[] = { 1,3,5,7,9,11 };
 
+    int size = sizeof(array) / sizeof(array[0]);
+    int element = 9;
+
+    std::cout << binarySearch(array, size, element) << std::endl;
 }
