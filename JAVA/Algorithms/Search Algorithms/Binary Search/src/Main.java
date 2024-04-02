b055cd8b7154167a34789a2603bebcc9e3501bcd
Luca Alfino <alfinoluca21@gmail.com>
2024-04-02 11:30:35 +0200

    Implemented Binary search java

--- JAVA/Algorithms/Search Algorithms/Binary Search/src/Main.java
+++ JAVA/Algorithms/Search Algorithms/Binary Search/src/Main.java
@@ -1,11 +1,39 @@
 public class Main {
     public static void main(String[] args) {
         int[] sortedArray = {1,3,5,7,9,11};
-        int element = 7;
+        int element = 11;
+
+        System.out.println(BinarySearch(sortedArray , element));
+
+        //Java also has a built in binary search algorithm inside
+        //of utils.Array package. I always recommend using built in
+        //packages since they are tested , efficient and overall integral.
     }
 
-    public static int BinarySearch(int array[] , int element)
+    public static int BinarySearch(int[] array, int element)
     {
+        int low = 0;
+        int high = array.length - 1;
 
+        while(low <= high)
+        {
+           int middleIndex = (high + low) / 2;
+           int middleValue = array[middleIndex];
+
+           if(element == middleValue)
+           {
+               return middleIndex;
+           }
+
+           if(element < middleValue)
+           {
+                high = middleIndex - 1;
+           }
+           else
+           {
+               low = middleIndex + 1;
+           }
+        }
+        return -1;
     }
 }
