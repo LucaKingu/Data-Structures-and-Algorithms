ab778e5d1160d32a5adfaea84b59bf6b1fec7fab
Luca Alfino <alfinoluca21@gmail.com>
2024-06-06 08:51:37 +0200

    Fixed python error and added conclusion

---
+++ PYTHON/Algorithms/Sorting Algorithms/Bubble Sort/Algorithmic Complexity/Conclusion.md
@@ -0,0 +1,5 @@
+Bubble sort is simple to implement but consists of a time complexity of Big O(n^2) which is highly inefficient. 
+
+It also consits of a space complexity of Big O(n) which means it requires a constant amount of space for temporary variables. 
+
+Thus, bubble sort should not be used in industry practice. 
--- PYTHON/Algorithms/Sorting Algorithms/Bubble Sort/main.py
+++ PYTHON/Algorithms/Sorting Algorithms/Bubble Sort/main.py
@@ -4,17 +4,16 @@ from matplotlib import pyplot as plt
 def bubbleSort(array):
     for i in range(len(array)):
         for j in range(len(array - i - 1)):
-            if(array[j] > array[j+1]):
-                temp = array[j]
-                array[j] = array[j+1]
-                array[j+1] = temp
+            if array[j] > array[j+1]:
+                #In python, you can use 'Tuple Unpacking'
+                array[j], array[j+1] = array[j+1], array[j]
 
 
 
 if __name__ == '__main__':
-    input_Array = {2,4,1,5,3}
+    input_Array = [10,20,30,40,50,60,70,80,90,100]
     #bubbleSort(input_Array)
-    runtimes = [1,2,3,4,5]
+    runtimes = [1,4,9,16,25,36,49,64,81,100]
 
     plt.plot(input_Array, runtimes, marker='o')
     plt.xlabel('List numbers')
