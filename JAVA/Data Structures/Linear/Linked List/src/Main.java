9a1b9edb640aa15fc34dee7d783f45c5558855c0
Luca Alfino <alfinoluca21@gmail.com>
2024-06-20 15:39:41 +0200

    Added Java insert linked list

---
+++ JAVA/Data Structures/Linear/Linked List/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        myLinkedList list = new myLinkedList();
+
+        list.insert(3);
+        list.insert(7);
+        list.insert(11);
+
+        list.show();
+    }
+}
